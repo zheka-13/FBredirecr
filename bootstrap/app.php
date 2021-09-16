2f74a916bc09fbc3d099b02944e0408f61eefcfb
zheka <poliv78@gmail.com>
2021-09-16 09:05:56 +0300

    sqlite db added, example env-file added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -61,6 +61,7 @@ $app->singleton(
 
 $app->configure('app');
 $app->configure('filesystems');
+$app->configure('database');
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'default' => env('DB_CONNECTION', 'sqlite'),
+    'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', ""),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+    ],
+];
